b749067201a84a00335b2ff7003627f4d8dc3ed6
Rodrigo Lima <rodrigofflima@hotmail.com>
2022-01-11 16:20:39 -0300

    initial commit

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import AdminJS from 'adminjs';
 import AdminJSExpress from '@adminjs/express';
+import express from 'express';
 
 const app = express();
 
@@ -7,4 +8,11 @@ const adminJS = new AdminJS({
   databases: [],
   rootPath: '/admin',
   resources: []
+});
+
+const router = AdminJSExpress.buildRouter(adminJS);
+
+app.use(adminJS.options.rootPath, router);
+app.listen(5001, () => {
+  console.log('AdminJS is under http://localhost:5001/admin');
 });
